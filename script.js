c6636d96c02b1bb39e73c958a7f5af8bdd9deb40
samirshrestha-tech <128810359+samirshrestha-tech@users.noreply.github.com>
2023-10-14 22:47:32 +1100

    update on ac, c buttons

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
-const displaElm = document.getElementsByClassName("display");
+const displaElm = document.getElementById("display");
+
 const allBtns = [...document.getElementsByClassName("btn")];
 let strToDisplay = "";
 
+displaElm.style.color = "red";
+
 const display = (str) => {
-  displaElm.innerText = str;
+  displaElm.innerHTML = str || "0.0";
 };
+//
+
+allBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const val = btn.innerText;
+    if (val === "AC") {
+      strToDisplay = "";
+      display(strToDisplay);
+      return;
+    }
+
+    if (val === "C") {
+      //   const lastValue = strToDisplay[strToDisplay.length - 1];
+      //   strToDisplay = lastValue;
+      strToDisplay = strToDisplay.slice(0, -1);
+      display(strToDisplay);
+      return;
+    }
+
+    strToDisplay += val;
+    display(strToDisplay);
+  });
+});
