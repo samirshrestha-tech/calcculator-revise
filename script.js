afc2c2d3757dc61919e92f57f55ee3630c5d34d4
samirshrestha-tech <128810359+samirshrestha-tech@users.noreply.github.com>
2023-10-15 23:32:46 +1100

    update on equal to operator

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const displaElm = document.getElementById("display");
 
 const allBtns = [...document.getElementsByClassName("btn")];
+const operators = ["+", "-", "*", "/", "%"];
 let strToDisplay = "";
+let lastOperator = "";
 
 displaElm.style.color = "red";
 
@@ -20,14 +22,28 @@ allBtns.forEach((btn) => {
     }
 
     if (val === "C") {
-      //   const lastValue = strToDisplay[strToDisplay.length - 1];
       //   strToDisplay = lastValue;
       strToDisplay = strToDisplay.slice(0, -1);
       display(strToDisplay);
       return;
     }
+    // this if condition is to check if the last char in the string is an operator and if it is then just take it out  when the equal to is pressed before doing the total operation.
+    if (val === "=") {
+      const lastValue = strToDisplay[strToDisplay.length - 1];
+      if (operators.includes(lastValue)) {
+        strToDisplay = strToDisplay.slice(0, -1);
+      }
+      return total();
+    }
+    // this if condition checks if the last char is operator and if it is then it doesn't allow other operators to be pressed consecutively.
 
     strToDisplay += val;
     display(strToDisplay);
   });
 });
+
+const total = () => {
+  const ttl = eval(strToDisplay);
+  display(ttl);
+  strToDisplay = ttl.toString();
+};
